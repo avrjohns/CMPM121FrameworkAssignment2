362eb6c6bf21403bc63a1d2d0c97a0e0f6d82f75
Nandini Iyer <Naiyer@ucsc.edu>
2026-05-10 23:51:10 -0700

    updated speedamp

--- Assets/Scripts/Spells/SpellBuilder.cs
+++ Assets/Scripts/Spells/SpellBuilder.cs
@@ -113,7 +113,7 @@ public class SpellBuilder
         switch (key)
         {
             case "damage_amp": return new DamageAmp(inner, owner, modData);
-            //case "speed_amp": return new SpeedAmp(inner, owner, modData);
+            case "speed_amp": return new SpeedAmp(inner, owner, modData);
            // case "doubler": return new Doubler(inner, owner, modData);
            // case "splitter": return new Splitter(inner, owner, modData);
            // case "chaos": return new Chaos(inner, owner, modData);
